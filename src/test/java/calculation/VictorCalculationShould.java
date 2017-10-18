e9f23c4c1dd8aaf228d9690bca9ff99e6e6f5a36
Javier Seixas <javier.seixas@scmspain.com>
2017-10-18 10:48:35 +0200

    Switch to parameterized test

--- src/test/java/calculation/VictorCalculationShould.java
+++ src/test/java/calculation/VictorCalculationShould.java
@@ -1,25 +1,39 @@
 package calculation;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import java.util.Arrays;
+import java.util.Collection;
 
 import static org.junit.Assert.*;
 
+@RunWith(Parameterized.class)
 public class VictorCalculationShould {
 
   VictorCalculation victorCalculation = new VictorCalculation();
+
+  @Parameterized.Parameters
+  public static Collection<Object[]> data() {
+    return Arrays.asList(new Object[][] {
+            { 20.0f, 96.4 }, { 30.0f, 91.9 }, { 50.0f, 77.5 }, { 70.0f, 55.9 }, { 80.0f, 42.4 }, { 90.0f, 27.1 }, { 100.0f, 10 }
+    });
+  }
+
+  @Parameterized.Parameter // first data value (0) is default
+  public /* NOT private */ float defeatedPercentage;
+
+  @Parameterized.Parameter(1)
+  public /* NOT private */ double expectedSurvivorsPercentage;
 
   @Test
   public void returnAccordingToTheFunction() throws Exception {
 
 
-    double result = this.victorCalculation.calculate(20.0f);
+    double result = this.victorCalculation.calculate(defeatedPercentage);
 
-    assertEquals(result, 96.4, 10);
-
-    result = this.victorCalculation.calculate(100.0f);
-
-    assertEquals(result, 10, 10);
-
+    assertEquals(result, expectedSurvivorsPercentage, 10);
   }
 
 }
